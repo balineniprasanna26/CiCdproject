cdd353104996222c772f17cf2b09b1c0c5727580
B Lakshmiprasanna <b.lakshmiprasanna@hcltech.com>
2025-07-11 14:18:06 +0530

    test appppp

--- test_app.py
+++ test_app.py
@@ -4,4 +4,5 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b'hi, have updated this' in response.data
+    assert b'hi4, have updated this' in response.data
+
